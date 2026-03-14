8f89fedb58aff0325f5852927b8a2a0944c70036
varisDogukan <varisdogukan@outlook.com>
2026-03-14 14:18:37 +0300

    style: add initial board column lane styles

--- src/features/boards/components/BoardColumn.tsx
+++ src/features/boards/components/BoardColumn.tsx
@@ -1,5 +1,6 @@
 import type { Column } from "../types/board.types";
 import TaskCard from "./TaskCard";
+import styles from "./BoardColumn.module.scss";
 
 type BoardColumnProps = {
   column: Column;
@@ -7,11 +8,13 @@ type BoardColumnProps = {
 
 export default function BoardColumn({ column }: BoardColumnProps) {
   return (
-    <article>
-      <h3>{column.name}</h3>
-      <p>{column.tasks.length}</p>
+    <article className={styles.root}>
+      <header className={styles.header}>
+        <h3 className={styles.title}>{column.name}</h3>
+        <p className={styles.taskCount}>{column.tasks.length}</p>
+      </header>
 
-      <div>
+      <div className={styles.tasks}>
         {column.tasks.map((task) => (
           <TaskCard key={task.id} task={task} />
         ))}
